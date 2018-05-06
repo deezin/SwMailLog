91626be502ef1c81788a970abcd2f48f36e9c7b0
Dennis Barthel <dennis@deezin.de>
2018-05-06 14:32:44 +0200

    Version 5.4, CronJob

--- DznMailLog.php
+++ DznMailLog.php
@@ -17,6 +17,7 @@ class DznMailLog extends Plugin
     {
         return [
             'Enlight_Components_Mail_Send' => 'onMailSend',
+            'Shopware_CronJob_PurgeMailLog' => 'onPurgeMailLogCronjob',
             'Enlight_Controller_Action_PreDispatch_Backend' => 'preparePlugin'
         ];
     }
@@ -85,14 +86,36 @@ class DznMailLog extends Plugin
         $mailLoggable->setFromMail($mail->getFrom());
         $mailLoggable->setSubject($mail->getPlainSubject());
 
-        if($mail->getBodyHtml()) {
-            $mailLoggable->setContent(str_replace("<div>", "<div style='white-space:initial;'>",$mail->getPlainBody()));
+        if ($mail->getBodyHtml()) {
+            $mailLoggable->setContent(str_replace("<div>", "<div style='white-space:initial;'>",
+                $mail->getPlainBody()));
         } else {
             $mailLoggable->setContent($mail->getPlainBodyText());
         }
 
         $entityManager->persist($mailLoggable);
         $entityManager->flush($mailLoggable);
+    }
+
+
+    public function onPurgeMailLogCronjob(\Shopware_Components_Cron_CronJob $job)
+    {
+        $config = $this->container->get('shopware.plugin.config_reader')->getByPluginName($this->getName());
+
+        if($config['maillog_interval'] != "0") {
+            $deleteMailInterval = $config['maillog_interval'];
+            $sql = "DELETE FROM s_dzn_maillog WHERE `time` < date_add(current_date, INTERVAL -{$deleteMailInterval} DAY)";
+            $result = Shopware()->Db()->query($sql);
+            if($result->rowCount() == 0) {
+                $data = 'Did not found any mails older than ' . $deleteMailInterval . ' day(s).';
+            } else {
+                $data = 'Deleted ' . $result->rowCount() . ' Mails from Mail Log.';
+            }
+        } else {
+            $data = "Did not delete any mails because interval value is 0.";
+        }
+
+        return $data;
     }
 
 }
